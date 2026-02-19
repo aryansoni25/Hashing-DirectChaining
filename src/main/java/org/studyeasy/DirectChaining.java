66693bfc5a23971ae5bfc66ea6952f673f8867e0
aryansoni25 <ashoksoni9425@gmail.com>
2026-02-19 15:26:18 +0530

    Initial commit

---
+++ src/main/java/org/studyeasy/DirectChaining.java
@@ -0,0 +1,62 @@
+package org.studyeasy;
+import java.util.ArrayList;
+import java.util.LinkedList;
+public class DirectChaining {
+    LinkedList<String>[] hashTable;
+    int maxChainSize=5;
+
+    DirectChaining(int size){
+        hashTable=new LinkedList[size];
+    }
+
+    public int modASCII(String word,int M){
+        char ch[];
+        ch=word.toCharArray();
+        int i,sum;
+        for(sum=0,i=0;i<word.length();i++){
+            sum=sum+ch[i];
+        }
+        return sum%M;
+    }
+
+    public void insertHashTable(String word){
+        int newIndex=modASCII(word,hashTable.length);
+        if(hashTable[newIndex]==null){
+            hashTable[newIndex]=new LinkedList<String>();
+            hashTable[newIndex].add(word);
+        }else{
+            hashTable[newIndex].add(word);
+        }
+    }
+
+    public void displayHashTable(){
+        if(hashTable==null){
+            System.out.println("Hash Table Doesn't exist");
+        }else{
+            System.out.println("\n---------------HashTable---------------");
+            for(int i=0;i<hashTable.length;i++){
+                System.out.println("Index: "+i+"Key: "+hashTable[i]+" ");
+            }
+        }
+    }
+
+    public boolean searchHashTable(String word){
+        int newIndex=modASCII(word,hashTable.length);
+        if(hashTable[newIndex]!=null && hashTable[newIndex].contains(word)){
+            System.out.println("\n"+"\""+word+"\""+" found in hashtable at location: "+newIndex);
+            return true;
+        }else{
+            System.out.println("\n"+"\""+word+"\""+" not found in hashtable at location");
+            return false;
+        }
+    }
+
+    public void deleteKeyHashTable(String word){
+        int newIndex=modASCII(word,hashTable.length);
+        boolean result=searchHashTable(word);
+        if(result){
+            hashTable[newIndex].remove();
+            System.out.println("\n"+"\""+word+"\""+" Word has been deleted from HashTable");
+        }
+    }
+}
---
+++ src/main/java/org/studyeasy/Main.java
@@ -0,0 +1,16 @@
+package org.studyeasy;
+
+public class Main {
+    public static void main(String[] args) {
+        DirectChaining dc=new DirectChaining(13);
+        dc.insertHashTable("The");
+        dc.insertHashTable("quick");
+        dc.insertHashTable("brown");
+        dc.insertHashTable("fox");
+        dc.insertHashTable("over");
+        dc.displayHashTable();
+        dc.searchHashTable("fox");
+        dc.deleteKeyHashTable("over");
+
+    }
+}
