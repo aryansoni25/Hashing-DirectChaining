2da88f3c1aad41fe26f81a5923d5c06a8c6a5d3c
aryansoni25 <ashoksoni9425@gmail.com>
2026-02-22 22:37:05 +0530

    update

--- src/main/java/org/studyeasy/Main.java
+++ src/main/java/org/studyeasy/Main.java
@@ -5,7 +5,7 @@ public class Main {
         DirectChaining dc=new DirectChaining(13);
         dc.insertHashTable("The");
         dc.insertHashTable("quick");
-        dc.insertHashTable("brown");
+        dc.insertHashTable("browny");
         dc.insertHashTable("fox");
         dc.insertHashTable("over");
         dc.displayHashTable();
